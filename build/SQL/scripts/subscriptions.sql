aed325014ca7ce6e8d3f8e4e1cbe025e81aea29d
zhichkin <dima_zhichkin@mail.ru>
2016-09-22 02:47:44 +0300

    sql scripts added

---
+++ build/SQL/scripts/subscriptions.sql
@@ -0,0 +1,15 @@
+﻿USE [Z];
+GO
+
+CREATE TABLE [integrator].[subscriptions](
+	[key] [uniqueidentifier] NOT NULL,
+	[version] [rowversion] NOT NULL,
+	[name] [nvarchar](100) NOT NULL,
+	[publisher] [uniqueidentifier] NOT NULL,
+	[subscriber] [uniqueidentifier] NOT NULL,
+ CONSTRAINT [pk_integrator_subscriptions] PRIMARY KEY CLUSTERED 
+(
+	[key] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
