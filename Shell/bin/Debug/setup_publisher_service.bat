f4f35f65aad7a5d8a43210e4bfd3298b0b5126d5
zhichkin <dima_zhichkin@mail.ru>
2016-09-22 18:53:58 +0300

    добавлены инструкции

---
+++ Shell/bin/Debug/setup_publisher_service.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+CLS
+C:\Users\User\Desktop\Z\Shell\bin\Debug\installutil.exe C:\Users\User\Desktop\Z\Shell\bin\Debug\Zhichkin.Integrator.PublisherAgent.exe
+PAUSE
---
+++ build/setup_subscriber_service.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+CLS
+C:\Users\User\Desktop\Z\build\installutil.exe /u C:\Users\User\Desktop\Z\build\Zhichkin.Integrator.SubscriberAgent.exe
+PAUSE
