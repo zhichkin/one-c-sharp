c897eefdcf359d5bdeefa34289ea44b76cc2830a
zhichkin <dima_zhichkin@mail.ru>
2019-02-19 02:14:11 +0300

    QueryView template

---
+++ Hermes.UI/CaseWhenThen/CaseWhenThenView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Zhichkin.Hermes.UI
+{
+    public partial class CaseWhenThenView : UserControl
+    {
+        //public static readonly DependencyProperty MyPropertyProperty;
+        //private static void OnMyPropertyChanged(DependencyObject target, DependencyPropertyChangedEventArgs args)
+        //{
+        //    CaseWhenThenView control = target as CaseWhenThenView;
+        //    if (control == null) return;
+        //    control.MyProperty = (args.NewValue == null ? null : (string)args.NewValue);
+        //}
+        //public string MyProperty
+        //{
+        //    set { SetValue(MyPropertyProperty, value); }
+        //    get { return (string)GetValue(MyPropertyProperty); }
+        //}
+        //static CaseWhenThenView()
+        //{
+        //    MyPropertyProperty = DependencyProperty.Register("MyProperty", typeof(string), typeof(CaseWhenThenView),
+        //        new FrameworkPropertyMetadata(
+        //            null,
+        //            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+        //            new PropertyChangedCallback(OnMyPropertyChanged)));
+        //}
+        public CaseWhenThenView()
+        {
+            CaseWhenThenViewModel viewModel = new CaseWhenThenViewModel()
+            {
+                Name = "CaseWhenThen Control"
+            };
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+    }
+}
---
+++ Hermes.UI/CaseWhenThen/FunctionTemplateSelector.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Zhichkin.Hermes.UI
+{
+    public sealed class FunctionTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement element = container as FrameworkElement;
+            if (element == null) return null;
+
+            return element.FindResource("CaseWhenThenTemplate") as DataTemplate;
+        }
+    }
+}
