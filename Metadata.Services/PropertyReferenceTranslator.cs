32111049ced18d7ce285b1b89db6a4fbe1c59874
zhichkin <dima_zhichkin@mail.ru>
2019-07-01 10:15:00 +0300

    QueryExecutor - start of work

---
+++ Metadata.Services/PropertyReferenceTranslator.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Data;
+using Zhichkin.Hermes.Model;
+using Zhichkin.Metadata.Model;
+
+namespace Zhichkin.Metadata.Services
+{
+    public sealed class PropertyReferenceTranslator
+    {
+        private readonly PropertyReference property;
+        public PropertyReferenceTranslator(PropertyReference property)
+        {
+            this.property = property;
+        }
+        public string ToSQL()
+        {
+            return $"[{property.Table.Alias}].[{property.Name}]";
+            //string valueType = property.Property.Relations.ToString();
+        }
+        //public object Translate(IDataReader source)
+        //{
+        //    if (_code == 0)
+        //    {
+        //        return TranslateComplexType(source);
+        //    }
+        //    else
+        //    {
+        //        return TranslateSingleType(source);
+        //    }
+        //}
+        //private object TranslateSingleType(IDataReader source)
+        //{
+        //    Entity type = Entity.Empty;
+        //    if (_code < 0)
+        //    {
+        //        type = InfoBase.Metadata.GetEntity(_code);
+        //    }
+        //    else // > 0 ReferenceObject
+        //    {
+        //        type = _metadata.GetEntity(_code);
+        //    }
+        //    int index = _ordinals[(int)FieldPurpose.Value];
+        //    if (index == -1) // Ссылка ???
+        //    {
+        //        index = _ordinals[(int)FieldPurpose.Object];
+        //    }
+        //    object value = source[index];
+        //    return ConvertValue(value, type);
+        //}
+        //private object TranslateComplexType(IDataReader source)
+        //{
+        //    byte[] buffer;
+        //    int locator = _ordinals[(int)FieldPurpose.Locator];
+        //    int typeCode = _ordinals[(int)FieldPurpose.TypeCode];
+        //    if (typeCode != -1)
+        //    {
+        //        buffer = (byte[])source[typeCode];
+        //        buffer = buffer.Reverse().ToArray();
+        //        typeCode = (int)_formatter.Deserialize(new MemoryStream(buffer), Entity.Int32);
+        //    }
+        //    int value = 0;
+        //    Entity type = null;
+        //    if (locator == -1) // ReferenceObject
+        //    {
+        //        type = _metadata.GetEntity(typeCode);
+        //        value = _ordinals[(int)FieldPurpose.Object];
+        //    }
+        //    else
+        //    {
+        //        buffer = (byte[])source[locator];
+        //        locator = (byte)_formatter.Deserialize(new MemoryStream(buffer), Entity.Byte);
+        //        if (locator == 1) // Неопределено
+        //        {
+        //            return null;
+        //        }
+        //        else if (locator == 2) // Булево
+        //        {
+        //            type = Entity.Boolean;
+        //            value = _ordinals[(int)FieldPurpose.Boolean];
+        //        }
+        //        else if (locator == 3) // Число
+        //        {
+        //            type = Entity.Decimal;
+        //            value = _ordinals[(int)FieldPurpose.Number];
+        //        }
+        //        else if (locator == 4) // Дата
+        //        {
+        //            type = Entity.DateTime;
+        //            value = _ordinals[(int)FieldPurpose.DateTime];
+        //        }
+        //        else if (locator == 5) // Строка
+        //        {
+        //            type = Entity.String;
+        //            value = _ordinals[(int)FieldPurpose.String];
+        //        }
+        //        else if (locator == 8) // Ссылка
+        //        {
+        //            type = _metadata.GetEntity(typeCode);
+        //            value = _ordinals[(int)FieldPurpose.Object];
+        //        }
+        //        else
+        //        {
+        //            return null;
+        //        }
+        //    }
+        //    return ConvertValue(source[value], type);
+        //}
+        //private object ConvertValue(object value, Entity type)
+        //{
+        //    if (value == DBNull.Value) return null;
+        //    if (value is byte[] && type != Entity.Binary)
+        //    {
+        //        return null; //_formatter.Deserialize(new MemoryStream((byte[])value), type);
+        //    }
+        //    else
+        //    {
+        //        return value;
+        //    }
+        //}
+    }
+}
---
+++ Metadata.Services/QueryExecutor.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using Zhichkin.Hermes.Model;
+
+namespace Zhichkin.Metadata.Services
+{
+    public sealed class QueryExecutor
+    {
+        private Dictionary<string, PropertyReferenceTranslator> propertyTranslators = new Dictionary<string, PropertyReferenceTranslator>();
+
+        public void BuildSQLCommand(QueryExpression query)
+        {
+            foreach (HermesModel expression in query.Expressions)
+            {
+                VisitExpression(expression);
+            }
+        }
+        private void VisitExpression(HermesModel expression)
+        {
+            if (expression is SelectStatement)
+            {
+                VisitSelectStatement((SelectStatement)expression);
+            }
+        }
+        private void VisitSelectStatement(SelectStatement expression)
+        {
+            foreach (PropertyExpression property in expression.SELECT)
+            {
+                VisitPropertyExpression(property);
+            }
+            foreach (TableExpression table in expression.FROM)
+            {
+                VisitTableExpression(table);
+            }
+            VisitBooleanFunction(expression.WHERE);
+        }
+        private void VisitPropertyExpression(PropertyExpression expression)
+        {
+            PropertyReference property = expression.Expression as PropertyReference;
+            if (property == null) return;
+
+            PropertyReferenceTranslator translator = new PropertyReferenceTranslator(property);
+            propertyTranslators.Add(expression.Alias, translator);
+            string sql = $"{translator.ToSQL()} AS [{expression.Alias}]";
+        }
+        private void VisitTableExpression(TableExpression table)
+        {
+            // TODO
+        }
+        private void VisitBooleanFunction(BooleanFunction expression)
+        {
+            // TODO
+        }
+    }
+}
+
+//private void PrepareSelectEntityDataCommand(Subscription subscription, AggregateItem item, Guid aggregate, SqlCommand command)
+//{
+//    command.CommandType = CommandType.Text;
+//    command.CommandText = GetSelectEntityDataScript(subscription, item);
+//    command.Parameters.Clear();
+//    AddParameters(command, item, aggregate);
+//}
+//private string GetSelectEntityDataScript(Subscription subscription, AggregateItem item)
+//{
+//    string sql = "SELECT {0} FROM [{1}] WHERE {2};";
+
+//    string table = subscription.Publisher.Entity.MainTable.Name;
+//    StringBuilder fields = new StringBuilder();
+//    StringBuilder where = new StringBuilder();
+
+//    AddFields(fields, subscription);
+//    AddConditions(where, item.Connector);
+
+//    sql = string.Format(sql, fields.ToString(), table, where.ToString());
+
+//    return sql;
+//}
+//private void AddFields(StringBuilder fields, Subscription subscription)
+//{
+//    foreach (string name in subscription.Mappings.Keys)
+//    {
+//        if (fields.Length > 0) fields.Append(", ");
+//        fields.AppendFormat("[{0}]", name);
+//    }
+//}
+//private void AddConditions(StringBuilder where, Property connector)
+//{
+//    foreach (Field field in connector.Fields)
+//    {
+//        if (field.Purpose == FieldPurpose.Locator)
+//        {
+//            if (where.Length > 0) where.Append(" AND ");
+//            where.AppendFormat("[{0}] = @locator", field.Name);
+//        }
+//        if (field.Purpose == FieldPurpose.TypeCode)
+//        {
+//            if (where.Length > 0) where.Append(" AND ");
+//            where.AppendFormat("[{0}] = @code", field.Name);
+//        }
+//        if (field.Purpose == FieldPurpose.Object || field.Purpose == FieldPurpose.Value)
+//        {
+//            if (where.Length > 0) where.Append(" AND ");
+//            where.AppendFormat("[{0}] = @value", field.Name);
+//        }
+//    }
+//}
+//private void AddParameters(SqlCommand command, AggregateItem item, Guid aggregate)
+//{
+//    foreach (Field field in item.Connector.Fields)
+//    {
+//        if (field.Purpose == FieldPurpose.Locator)
+//        {
+//            command.Parameters.AddWithValue("locator", new byte[] { 0x08 }); // reference
+//        }
+//        if (field.Purpose == FieldPurpose.TypeCode)
+//        {
+//            command.Parameters.AddWithValue("code", Utilities.GetByteArray(item.Aggregate.Code));
+//        }
+//        if (field.Purpose == FieldPurpose.Object || field.Purpose == FieldPurpose.Value)
+//        {
+//            command.Parameters.AddWithValue("value", aggregate.ToByteArray());
+//        }
+//    }
+//}
